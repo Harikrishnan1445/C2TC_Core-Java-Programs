3323e013970530117054b309641adece1d868589
gokul <gokul@HK>
2025-10-03 19:36:36 +0530

    Day3

---
+++ corejava/src/day2/ForLoop.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class ForLoop {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=100 ; i++)  
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+	}
+
+}
